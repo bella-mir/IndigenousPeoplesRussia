7f8650c0defec88871832a83172942511472c72b
Bella Mironova <belkamirbella@yandex.ru>
2022-01-04 21:26:07 +0300

    Updates

---
+++ precache-manifest.67e81ad57550172a11893088ebab1275.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4ad920f1146caa21d9c128908764bb",
+    "url": "/IndigenousPeoplesRussia/index.html"
+  },
+  {
+    "revision": "ae5754b58db9b2d8305e",
+    "url": "/IndigenousPeoplesRussia/static/css/2.67697698.chunk.css"
+  },
+  {
+    "revision": "4658243e91ff605f400b",
+    "url": "/IndigenousPeoplesRussia/static/css/main.02bb96a7.chunk.css"
+  },
+  {
+    "revision": "ae5754b58db9b2d8305e",
+    "url": "/IndigenousPeoplesRussia/static/js/2.94ccda78.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/IndigenousPeoplesRussia/static/js/2.94ccda78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4658243e91ff605f400b",
+    "url": "/IndigenousPeoplesRussia/static/js/main.1ab720ae.chunk.js"
+  },
+  {
+    "revision": "d6acbc29908fb919661d",
+    "url": "/IndigenousPeoplesRussia/static/js/runtime-main.82925903.js"
+  },
+  {
+    "revision": "e5e9d9988322e88ae8177f6351cc076a",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Bold.e5e9d998.woff"
+  },
+  {
+    "revision": "61627fcd9cabb2d3df34136db5e244cd",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-BoldItalic.61627fcd.woff"
+  },
+  {
+    "revision": "c69cfb3bef4bc9a58d65a37d4f3307e8",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-ExtraBold.c69cfb3b.woff"
+  },
+  {
+    "revision": "a8b4cd02630953924ea33e69367f531c",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-ExtraBoldItalic.a8b4cd02.woff"
+  },
+  {
+    "revision": "3d6ea74c935f34c39b1e0c51ebb8980c",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-ExtraLight.3d6ea74c.woff"
+  },
+  {
+    "revision": "66c6f32caf93b33c107f0438e5315d16",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-ExtraLightItalic.66c6f32c.woff"
+  },
+  {
+    "revision": "9f75328424b6c88934c96797386bae60",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Heavy.9f753284.woff"
+  },
+  {
+    "revision": "39f25f1ff7bfae893636115c62bd0179",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-HeavyItalic.39f25f1f.woff"
+  },
+  {
+    "revision": "538456e7c4e382f1231fd5045debfa99",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Italic.538456e7.woff"
+  },
+  {
+    "revision": "5fd8e0ca2185c97d676e6c8b12f1d5cf",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Light.5fd8e0ca.woff"
+  },
+  {
+    "revision": "597a23b93339c13b2efd9da59481dc3e",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-LightItalic.597a23b9.woff"
+  },
+  {
+    "revision": "bc1db4f4e2940883da073147c66d4d5d",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Medium.bc1db4f4.woff"
+  },
+  {
+    "revision": "10cb876e9cd7830224943d2fe7b1c1e3",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-MediumItalic.10cb876e.woff"
+  },
+  {
+    "revision": "d2cd158b4bb3a19b974dd9aeb03fa0f0",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-Thin.d2cd158b.woff"
+  },
+  {
+    "revision": "393bfeca6cc83027d0a18c73bf71ea06",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria-ThinItalic.393bfeca.woff"
+  },
+  {
+    "revision": "c7b2c2d8ad8b847c51a6e2609d910ae4",
+    "url": "/IndigenousPeoplesRussia/static/media/Geometria.c7b2c2d8.woff"
+  }
+]);
